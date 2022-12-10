4b2102cddec45e5978f5c9de35ce8cd630fe3a04
Renamethis <ops282@mail.ru>
2022-12-10 03:10:46 +0300

    Update enviro_server script
    Add dataclasses for sensor values,
    Update information representation on lcd screen

--- enviro_server.py
+++ enviro_server.py
@@ -18,7 +18,23 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 from fonts.ttf import RobotoMedium as UserFont
+from dataclasses import dataclass, fields
 import logging
+
+@dataclass
+class EnvironmentValue:
+    value: float
+    unit: str
+
+@dataclass
+class EnvironmentData:
+        temperature : EnvironmentValue # C
+        pressure : EnvironmentValue # hPa
+        humidity : EnvironmentValue # %
+        illumination : EnvironmentValue # lux
+        oxidising : EnvironmentValue # Ok
+        reducing : EnvironmentValue # Ok
+        nh3 : EnvironmentValue # Ok
 
 logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
@@ -52,7 +68,7 @@ HEIGHT = st7735.height
 img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
 draw = ImageDraw.Draw(img)
 path = os.path.dirname(os.path.realpath(__file__))
-font_size = 20
+font_size = 11
 font = ImageFont.truetype(UserFont, font_size)
 
 message = ""
@@ -62,28 +78,15 @@ top_pos = 25
 
 
 # Displays data and text on the 0.96" LCD
-def display_text(variable, data, unit):
-    # Maintain length of list
-    values[variable] = values[variable][1:] + [data]
-    # Scale the values for the variable between 0 and 1
-    vmin = min(values[variable])
-    vmax = max(values[variable])
-    colours = [(v - vmin + 1) / (vmax - vmin + 1) for v in values[variable]]
-    # Format the variable name and value
-    message = "{}: {:.1f} {}".format(variable[:4], data, unit)
-    logging.info(message)
+def display(data):
+    y = 0
     draw.rectangle((0, 0, WIDTH, HEIGHT), (255, 255, 255))
-    for i in range(len(colours)):
-        # Convert the values to colours from red to blue
-        colour = (1.0 - colours[i]) * 0.6
-        r, g, b = [int(x * 255.0) for x in colorsys.hsv_to_rgb(colour, 1.0, 1.0)]
-        # Draw a 1-pixel wide rectangle of colour
-        draw.rectangle((i, top_pos, i + 1, HEIGHT), (r, g, b))
-        # Draw a line graph in black
-        line_y = HEIGHT - (top_pos + (colours[i] * (HEIGHT - top_pos))) + top_pos
-        draw.rectangle((i, line_y, i + 1, line_y + 1), (0, 0, 0))
-    # Write the text at the top in black
-    draw.text((0, 0), message, font=font, fill=(0, 0, 0))
+    for field in fields(data):
+        name = field.name
+        print(getattr(data, field.name))
+        message = field.name + " = %.1f" %  getattr(data, field.name).value + " %s" % getattr(data, field.name).unit
+        draw.text((0, y), message, font=font, fill=(0, 0, 0))
+        y+=11
     st7735.display(img)
 
 
@@ -99,23 +102,13 @@ def get_cpu_temperature():
 # temperature down, and increase to adjust up
 factor = 2.25
 
-cpu_temps = [get_cpu_temperature()] * 5
-
 delay = 0.5  # Debounce the proximity tap
 mode = 0  # The starting mode
 last_page = 0
 light = 1
 
-# Create a values dict to store the data
-variables = ["temperature",
-             "pressure",
-             "humidity",
-             "light",
-             "oxidised",
-             "reduced",
-             "nh3"]
-
 def get_compensated_temperature():
+    cpu_temps = [get_cpu_temperature()] * 5
     cpu_temp = get_cpu_temperature()
     cpu_temps = cpu_temps[1:] + [cpu_temp]
     avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
@@ -125,44 +118,23 @@ def get_compensated_temperature():
 # The main loop
 try:
     while True:
-        temperature = get_compensated_temperature()
+        temperature = get_compensated_temperature() # C
         pressure = bme280.get_pressure() # hPa
+        humidity = bme280.get_humidity() # %
+        illumination = ltr559.get_lux() # lux
+        gas_data = gas.read_all() # Ok
+        oxidising = gas_data.oxidising / 1000
+        reducing = gas_data.reducing / 1000
+        nh3 = gas_data.nh3 / 1000
+        display(EnvironmentData(
+            EnvironmentValue(temperature, "C"), 
+            EnvironmentValue(pressure, "hPa"), 
+            EnvironmentValue(humidity, "%"), 
+            EnvironmentValue(illumination, "Lux"), 
+            EnvironmentValue(oxidising, "Ol"), 
+            EnvironmentValue(reducing, "Ok"), 
+            EnvironmentValue(nh3, "Ok")))
 
-        if mode == 2:
-            # variable = "humidity"
-            unit = "%"
-            data = bme280.get_humidity()
-            display_text(variables[mode], data, unit)
-
-        if mode == 3:
-            # variable = "light"
-            unit = "Lux"
-            if proximity < 10:
-                data = ltr559.get_lux()
-            else:
-                data = 1
-            display_text(variables[mode], data, unit)
-
-        if mode == 4:
-            # variable = "oxidised"
-            unit = "kO"
-            data = gas.read_all()
-            data = data.oxidising / 1000
-            display_text(variables[mode], data, unit)
-
-        if mode == 5:
-            # variable = "reduced"
-            unit = "kO"
-            data = gas.read_all()
-            data = data.reducing / 1000
-            display_text(variables[mode], data, unit)
-
-        if mode == 6:
-            # variable = "nh3"
-            unit = "kO"
-            data = gas.read_all()
-            data = data.nh3 / 1000
-            display_text(variables[mode], data, unit)
 
 # Exit cleanly
 except KeyboardInterrupt:
