2d38d3831521bc599b1e6e5681fe4c94040c71cc
Renamethis <ops282@mail.ru>
2023-07-22 00:15:23 +0300

    Add updating database with new data in celery tasks

--- enviro_server/tasks.py
+++ enviro_server/tasks.py
@@ -4,7 +4,7 @@ from celery.signals import task_prerun
 import os
 from celery.signals import worker_ready
 from .EnvironmentData import Units, EnvironmentData
-from .database.models import EnvironmentUnitModel
+from .database.models import EnvironmentUnitModel, EnvironmentRecordModel
 from celery.utils.log import get_task_logger
 from .EnvironmentThread import EnvironmentInterface
 
@@ -12,7 +12,7 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 celery.conf.beat_schedule = {
     'update_task': {
         'task': 'enviro_server.tasks.update_data_from_sensors',
-        'schedule': 1.0,
+        'schedule': 10.0,
     },
 }
 celery.conf.timezone = 'UTC'
@@ -25,7 +25,7 @@ def initialize(sender, **k):
         app.interface.start()
         for unit in Units:
             new_entry = EnvironmentUnitModel(
-                field_name = unit.name.lower(),
+                type = unit.name.lower(),
                 unit = unit.value
             )
             db.session.add(new_entry)
@@ -35,7 +35,24 @@ def initialize(sender, **k):
 @celery.task
 def update_data_from_sensors():
     with app.app_context():
-        logger.info("TEST")
         data = redis_client.lrange('Data', -1, -1)
-        data = EnvironmentData.from_message(data[0])
-        print(data.serialize())
+        data = EnvironmentData.from_message(data[0]).get_dict()
+        last = EnvironmentRecordModel.query. \
+            order_by(EnvironmentRecordModel.id.desc()).first()
+        current_id = 0 if last is None else last.id + 1
+        time = data['datetime']
+        for key in data:
+            if(key == 'datetime'):
+                continue
+            unit = EnvironmentUnitModel.query.\
+                   filter_by(type = data[key]['unit'].name.lower())[0].type
+            new_entry = EnvironmentRecordModel(
+                id=current_id,
+                ptime=time,
+                field_name=key,
+                unit=unit,
+                value=data[key]['value'],
+            )
+            db.session.add(new_entry)
+            current_id += 1
+        db.session.commit()
