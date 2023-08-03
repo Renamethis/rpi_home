945f89fd19711521fa27384e5e8d0302561ac25c
Renamethis <ops282@mail.ru>
2023-08-03 03:09:42 +0300

    Add colours dict

---
+++ front/src/Colorus.js
@@ -0,0 +1,11 @@
+
+export const colours = {
+    "temperature": "rgb(200, 100, 255)",
+    "pressure": "rgb(0, 50, 200)",
+    "humidity": "rgb(0, 25, 150)",
+    "illumination" : "rgb(200, 200, 0)",
+    "dust" : "rgb(100, 100, 100)",
+    "oxidising" : "rgb(200, 20, 0)",
+    "reducing" : "rgb(100, 200, 0)",
+    "nh3": "rgb(50, 200, 200)",
+};
