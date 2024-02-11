5dca3f1183ba7c7d335082fd2c3e4a4dc285f052
Renamethis <ops282@mail.ru>
2024-02-12 02:17:23 +0300

    Add last_entries task test

--- enviro_server/tests/tasks_test.py
+++ enviro_server/tests/tasks_test.py
@@ -4,6 +4,7 @@ import pytest
 from celery.exceptions import Retry
 from unittest.mock import patch
 from enviro_server.tasks import last_entries
+from enviro_server.EnvironmentData import CHANNELS
 
 @pytest.fixture(scope='session')
 def celery_config():
@@ -12,7 +13,16 @@ def celery_config():
         'result_backend': os.getenv("REDIS_URL")
     }
 
+# TODO: Add testset with pointer bigger than amount
+last_entries_test_set = ((20, 50), (2, 5))
+
 def test_last_entries():
-    test_pointer = 0
-    test_amount = 10
-    assert last_entries.delay([int(test_pointer), int(test_amount),]).get() != 5
+    for test_set in last_entries_test_set:
+        __test_last_entries(test_set[0], test_set[1])
+
+def __test_last_entries(pointer, amount):
+    result = last_entries.delay([int(pointer), int(amount),]).get()
+    assert result is not None
+    assert len(result) < amount * CHANNELS
+    for entry in result:
+        assert entry is not None
