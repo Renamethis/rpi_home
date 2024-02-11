cd822c1d32004eafbbe91ff79fa0bd150604ebd0
Renamethis <ops282@mail.ru>
2024-02-11 15:11:06 +0300

    Add check for test environment in __init__.py

--- enviro_server/__init__.py
+++ enviro_server/__init__.py
@@ -1,14 +1,16 @@
+import os
 from flask import Flask
 from config import config
 from .extensions import db, celery, cors
 
-# Flask factory
-def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
-    db.init_app(app)
-    celery.init_app(app)
-    cors.init_app(app, resources={r"*": {"origins": "*"}})
-    app.config['CORS_HEADERS'] = 'application/json'
-    return app
+if os.getenv("TEST_ENVIRONMENT") is None:
+    # Flask factory
+    def create_app(config_name):
+        app = Flask(__name__)
+        app.config.from_object(config[config_name])
+        config[config_name].init_app(app)
+        db.init_app(app)
+        celery.init_app(app)
+        cors.init_app(app, resources={r"*": {"origins": "*"}})
+        app.config['CORS_HEADERS'] = 'application/json'
+        return app
