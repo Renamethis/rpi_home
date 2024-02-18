f06dc526c667d151062423ede11a9bf2a1e032fe
Renamethis <ops282@mail.ru>
2024-02-18 22:35:12 +0300

    Add test_by_date, test_current_state celery tests

--- enviro_server/tests/tasks_test.py
+++ enviro_server/tests/tasks_test.py
@@ -1,10 +1,13 @@
 
 import os
 import pytest
-from celery.exceptions import Retry
+import logging
+from datetime import datetime
 from unittest.mock import patch
-from enviro_server.tasks import last_entries
-from enviro_server.EnvironmentData import CHANNELS
+from enviro_server.tasks import last_entries, current_state, by_date
+from enviro_server.EnvironmentData import CHANNELS, Units, Limits
+
+LOGGER = logging.getLogger(__name__)
 
 @pytest.fixture(scope='session')
 def celery_config():
@@ -16,9 +19,50 @@ def celery_config():
 # TODO: Add testset with pointer bigger than amount
 last_entries_test_set = ((20, 50), (2, 5))
 
+def test_current_state():
+    result = current_state.delay().get()
+    assert len(result) != 0
+    assert len(result) == CHANNELS + 1
+    for key in result.keys():
+        __test_record(key, result[key], True)
+
+def test_by_date():
+    date = last_entries.delay([0, 1, ]).get()[0]['datetime']
+    assert date is not None
+    result = by_date.delay([date, ]).get()
+    assert result is not None
+    assert len(result) == CHANNELS + 1
+    for key in result:
+        __test_record(key, result[key])
+
+def test_update_data_from_sensors():
+    raise NotImplementedError
+
 def test_last_entries():
     for test_set in last_entries_test_set:
         __test_last_entries(test_set[0], test_set[1])
+
+def __test_record(key, value, precise=False):
+    assert key is not None
+    assert value is not None
+    if(key != 'datetime'):
+        assert value['unit'] in iter(Units)
+        assert tuple(value['limits']) in Limits.values()
+    else:
+        __test_datetime(value, precise)
+
+def __test_datetime(value, precise=False):
+    try:
+        time = datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f')
+    except ValueError:
+        time = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+    assert time is not None
+    if(precise):
+        assert datetime.now().year == time.year
+        assert datetime.now().month == time.month
+        assert datetime.now().day == time.day
+        assert datetime.now().hour == time.hour
+
 
 def __test_last_entries(pointer, amount):
     result = last_entries.delay([int(pointer), int(amount),]).get()
