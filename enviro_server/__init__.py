f116c91f16cb3e590d31695ceb6440c64b542121
Renamethis <ops282@mail.ru>
2024-02-19 23:54:17 +0300

    Add config.ini file for server configuration

--- enviro_server/__init__.py
+++ enviro_server/__init__.py
@@ -2,10 +2,14 @@ import os
 from flask import Flask
 from config import config
 from .extensions import db, celery, cors
+import configparser
 
 # Flask factory
 def create_app(config_name):
     app = Flask(__name__)
+    config = configparser.ConfigParser()
+    config.read("enviro_server/config.ini")
+    app.app_config = config
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     db.init_app(app)
