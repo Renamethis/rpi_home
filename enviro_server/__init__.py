8dfd9e9c8118eaff0403c746a89d9532c94c09b1
Renamethis <ops282@mail.ru>
2024-03-04 01:43:51 +0300

    Replace app creation to avoid circular imports

--- enviro_server/__init__.py
+++ enviro_server/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from enviro_server.auth.auth import auth
 from config import config
 from .extensions import db, celery, cors
 import configparser
@@ -9,11 +10,14 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    db.init_app(app)
     celery.init_app(app)
     cors.init_app(app, resources={r"*": {"origins": "*"}})
     app.config['CORS_HEADERS'] = 'application/json'
     app_config = configparser.ConfigParser()
     app_config.read("enviro_server/config.ini")
+    app.register_blueprint(auth)
     app.app_config = app_config
+    db.init_app(app)
     return app
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
