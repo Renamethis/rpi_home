bacc3424fbcb9eaf803ac6ce42872c3857a4c204
Renamethis <ops282@mail.ru>
2024-03-04 01:43:16 +0300

    Add login, signup with token tasks

--- enviro_server/auth/tasks.py
+++ enviro_server/auth/tasks.py
@@ -1,23 +1,77 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
+import jwt
+from flask import make_response, jsonify
 from enviro_server.extensions import celery, db
 from enviro_server.database.models import User
 
-@celery.task()
+@celery.task
+def decode_token(auth_token):
+    try:
+        payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
+
+@celery.task
+def login_task(args):
+    request = args[0]
+    post_data = request.get_json()
+    try:
+        # fetch the user data
+        user = User.query.filter_by(
+            email=post_data.get('email')
+            ).first()
+        auth_token = user.encode_auth_token(user.id)
+        if auth_token:
+            responseObject = {
+                'status': 'success',
+                'message': 'Successfully logged in.',
+                'auth_token': auth_token.decode()
+            }
+            return make_response(jsonify(responseObject)), 200
+    except Exception as e:
+        print(e)
+        responseObject = {
+            'status': 'fail',
+            'message': 'Try again'
+        }
+        return make_response(jsonify(responseObject)), 500
+
+
+@celery.task
 def signup_task(args):
     request = args[0]
-    nickname = request.form.get('nickname')
-    full_name = request.form.get('full_name')
-    password = request.form.get('password')
+    post_data = request.get_json()
+    # check if user already exists
+    user = User.query.filter_by(email=post_data.get('email')).first()
+    if not user:
+        try:
+            user = User(
+                nickname=post_data.get('email'),
+                password=post_data.get('password')
+            )
 
-    user = User.query.filter_by(nickname=nickname).first()
-
-    if user:
-        return None
-
-    new_user = User(nickname=nickname, full_name=full_name, password=generate_password_hash(password, method='sha256'))
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return new_user
+            # insert the user
+            db.session.add(user)
+            db.session.commit()
+            # generate the auth token
+            auth_token = user.encode_auth_token(user.id)
+            responseObject = {
+                'status': 'success',
+                'message': 'Successfully registered.',
+                'auth_token': auth_token.decode()
+            }
+            return make_response(jsonify(responseObject)), 201
+        except Exception as e:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Some error occurred. Please try again.'
+            }
+            return make_response(jsonify(responseObject)), 401
+    else:
+        responseObject = {
+            'status': 'fail',
+            'message': 'User already exists. Please Log in.',
+        }
+        return make_response(jsonify(responseObject)), 202
