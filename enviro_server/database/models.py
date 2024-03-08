66bbc7bd43c4c03c442011892a8ec61727cc8431
Renamethis <ops282@mail.ru>
2024-03-08 04:58:35 +0300

    Fix jwt encoding/decoding issues

--- enviro_server/database/models.py
+++ enviro_server/database/models.py
@@ -47,19 +47,19 @@ class User(db.Model):
     @staticmethod
     def decode_auth_token(auth_token):
         try:
-            payload = jwt.decode(auth_token, "TEST_KEY")
+            payload = jwt.decode(auth_token, "TEST_KEY", algorithms=["HS256"])
             return payload['sub']
         except jwt.ExpiredSignatureError:
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
             return 'Invalid token. Please log in again.'
 
-    def encode_auth_token(self, user_id):
+    def encode_auth_token(self, nickname):
         try:
             payload = {
                 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
                 'iat': datetime.datetime.utcnow(),
-                'sub': user_id
+                'sub': nickname
             }
             return jwt.encode(
                 payload,
