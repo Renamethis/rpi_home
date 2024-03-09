bb8be0930374d894e22e33e21ced3aa2f705e386
Renamethis <ops282@mail.ru>
2024-03-09 03:42:55 +0300

    Add test utils for data loading

--- enviro_server/tests/unittests/DataTest.py
+++ enviro_server/tests/unittests/DataTest.py
@@ -1,13 +1,8 @@
 import unittest
 from json import load
+from enviro_server.tests.utils.load_environment_resources import load_environment_data
 from enviro_server.database.models import EnvironmentRecordModel
 
 class DataTest(unittest.TestCase):
         def setUp(self):
-            with open(self.resource_path, "r") as resource:
-                dict_data = load(resource)
-                self._raw_data = dict_data
-                self._data = [EnvironmentRecordModel(id=entry["id"], ptime=entry["ptime"],
-                                                     value=entry["value"], field_name=entry["field_name"],
-                                                     unit=entry["unit"])
-                              for entry in dict_data]
+            self._raw_data, self._data = load_environment_data(self.resource_path)
